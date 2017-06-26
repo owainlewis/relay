a6805abebefaec694070bba40386d84650418126
Owain Lewis <owain@owainlewis.com>
2017-06-26 10:07:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,5 @@ RELEASE=0.1
 
 wget https://github.com/owainlewis/relay/releases/download/$RELEASE/relay
 
-chmod +x relay
-
-mv relay /usr/local/bin/
-
-echo 'Done'
+mv relay /usr/local/bin/ && \
+chmod +x /usr/local/bin/relay
