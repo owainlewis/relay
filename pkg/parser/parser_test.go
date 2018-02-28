e05dbbab9a4e036b8e476c5e3716c7a150e31cf9
Owain Lewis <owain@owainlewis.com>
2018-02-28 21:44:23 +0000

    Fix test relative paths

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestParserGet(t *testing.T) {
 	params := map[string]string{"method": "get"}
-	_, err := ParseFile("../examples/get.yml", params)
+	_, err := ParseFile("../../examples/get.yml", params)
 	if err != nil {
 		t.Error("Cannot parse file get.yaml")
 	}
@@ -14,7 +14,7 @@ func TestParserGet(t *testing.T) {
 
 func TestDefaultOptions(t *testing.T) {
 	params := map[string]string{"method": "get"}
-	req, _ := ParseFile("../examples/client-opts.yml", params)
+	req, _ := ParseFile("../../examples/client-opts.yml", params)
 	if req.Options.Timeout != 20 {
 		t.Error("Expecting timeout value of 20 seconds for HTTP client options")
 	}
